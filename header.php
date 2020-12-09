5567052f276110013ac2d749b69461009c286386
Nick Gaswirth <admin@roundhouse-designs.com>
2020-12-09 16:35:45 -0500

    working

--- header.php
+++ header.php
@@ -28,14 +28,11 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
+			if ( has_custom_logo() ) :
+				the_custom_logo();
 			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			endif;
 			$rhd_novo_description = get_bloginfo( 'description', 'display' );
@@ -52,6 +49,7 @@
 				array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
+					'menu_class' => 'primary-menu'
 				)
 			);
 			?>
