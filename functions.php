8adf6a63d07040d0c3bc6a9a2f5f1db4f1a84d3e
Nick Gaswirth <admin@roundhouse-designs.com>
2020-12-09 02:51:55 -0500

    palette support

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if ( ! defined( 'RHD_NOVO_VERSION' ) ) {
 	define( 'RHD_NOVO_VERSION', '1.0.0' );
 }
 
-if ( ! function_exists( 'rhd_novo_setup' ) ) :
+if ( ! function_exists( 'rhd_novo_setup' ) ):
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -22,29 +22,29 @@ if ( ! function_exists( 'rhd_novo_setup' ) ) :
 	 */
 	function rhd_novo_setup() {
 		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on RHD_Novo, use a find and replace
-		 * to change 'rhd-novo' to the name of your theme in all the template files.
-		 */
+			 * Make theme available for translation.
+			 * Translations can be filed in the /languages/ directory.
+			 * If you're building a theme based on RHD_Novo, use a find and replace
+			 * to change 'rhd-novo' to the name of your theme in all the template files.
+		*/
 		load_theme_textdomain( 'rhd-novo', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
 		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
+			 * Let WordPress manage the document title.
+			 * By adding theme support, we declare that this theme does not use a
+			 * hard-coded <title> tag in the document head, and expect WordPress to
+			 * provide it for us.
+		*/
 		add_theme_support( 'title-tag' );
 
 		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
+			 * Enable support for Post Thumbnails on posts and pages.
+			 *
+			 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		*/
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
@@ -55,9 +55,9 @@ if ( ! function_exists( 'rhd_novo_setup' ) ) :
 		);
 
 		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
+			 * Switch default core markup for search form, comment form, and comments
+			 * to output valid HTML5.
+		*/
 		add_theme_support(
 			'html5',
 			array(
@@ -150,6 +150,56 @@ function rhd_novo_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'rhd_novo_scripts' );
+
+/**
+ * Gutenberg Support. Read our compiled theme CSS and extract the WP colour palette so we can register it with Gutenberg.
+ */
+function rhd_novo_gutenberg_support() {
+	// Update the css path to your plugin's css file.
+	$compiled_css_path = get_template_directory() . '/style.css';
+
+	$cache_key = md5_file( $compiled_css_path );
+	$cached    = get_option( '__theme_cached_color_palette_version', false );
+	if ( $cached == $cache_key ) {
+		$colour_palette = get_option( '__theme_cached_color_palette', [] );
+	} else {
+		$theme_css = file_get_contents( $compiled_css_path );
+		preg_match_all( '/\.has-([^}]*)-background-color\s*{\s*background-color[^\S\r\n]*:[^\S\r\n]*([^};]*);?\s*}/', $theme_css, $matches );
+		$colour_palette = [];
+		$assigned       = [];
+		if ( is_array( $matches ) && isset( $matches[0] ) && isset( $matches[1] ) && isset( $matches[2] ) ) {
+			// $full_match = $matches[0]; // The full matched string
+			$colour_slugs  = $matches[1]; // The colour slug pulled from .has-(\S+)-background-color
+			$colour_values = $matches[2]; // The colour value
+			if ( is_array( $colour_slugs ) && is_array( $colour_slugs ) ) {
+				foreach ( $colour_slugs as $index => $slug ) {
+					if ( ! isset( $colour_values[$index] ) ) {
+						continue;
+					}
+
+					$colour_val = trim( $colour_values[$index] ); // Important to trim whitespace from regex extraction.
+					if ( in_array( $colour_val, $assigned ) ) {
+						continue;
+					}
+
+					$assigned[]       = $colour_val;
+					$colour_palette[] = [
+						'name'  => ucwords( str_replace( ['-', '_'], ' ', $slug ) ),
+						'slug'  => $slug,
+						'color' => $colour_val,
+					];
+				}
+			}
+			update_option( '__theme_cached_color_palette_version', $cache_key );
+			update_option( '__theme_cached_color_palette', $colour_palette );
+		}
+	}
+	if ( $colour_palette ) {
+		add_theme_support( 'disable-custom-colors' );
+		add_theme_support( 'editor-color-palette', $colour_palette );
+	}
+}
+add_action( 'after_setup_theme', 'rhd_novo_gutenberg_support', 20 );
 
 /**
  * Implement the Custom Header feature.
