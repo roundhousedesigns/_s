ba95be65b1faccb30f1e015bc83cbdd1bdb95e35
Nick Gaswirth <admin@roundhouse-designs.com>
2020-12-11 03:36:58 -0500

    Livereload finished

--- dev/livereload.js
+++ dev/livereload.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 var livereload = require("livereload");
 var server = livereload.createServer({
 	port: 35729,
-	https: {
-		key: fs.readFileSync('/srv/rhdwp/ssl/localhost.key'),
-		cert: fs.readFileSync('/srv/rhdwp/ssl/localhost.crt')
-	},
+	// https: {
+	// 	key: fs.readFileSync('/srv/rhdwp/ssl/localhost.key'),
+	// 	cert: fs.readFileSync('/srv/rhdwp/ssl/localhost.crt')
+	// },
 	exts: ["html", "css", "js", "php"],
 });
 server.watch(".");
