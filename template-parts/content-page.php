b47006d7616b0f461f65e16a80e06b38812a4230
Nick Gaswirth <admin@roundhouse-designs.com>
2021-10-05 18:43:42 -0400

    tweaks

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header default-max-width">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title page-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<?php rhd_post_thumbnail(); ?>
