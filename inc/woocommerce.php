f54795625486bebbbe4aab0a1c02d37a161be6e0
Nick Gaswirth <nick@roundhouse-designs.com>
2021-10-07 12:56:31 -0400

    removed weird woo styles

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -38,30 +38,6 @@ function rhd_woocommerce_setup() {
 add_action( 'after_setup_theme', 'rhd_woocommerce_setup' );
 
 /**
- * WooCommerce specific scripts & stylesheets.
- *
- * @return void
- */
-function rhd_woocommerce_scripts() {
-	wp_enqueue_style( 'rhd-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), RHD_VERSION );
-
-	// $font_path   = WC()->plugin_url() . '/assets/fonts/';
-	// $inline_font = '@font-face {
-	// 		font-family: "star";
-	// 		src: url("' . $font_path . 'star.eot");
-	// 		src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
-	// 			url("' . $font_path . 'star.woff") format("woff"),
-	// 			url("' . $font_path . 'star.ttf") format("truetype"),
-	// 			url("' . $font_path . 'star.svg#star") format("svg");
-	// 		font-weight: normal;
-	// 		font-style: normal;
-	// 	}';
-
-	// // wp_add_inline_style( 'rhd-woocommerce-style', $inline_font );
-}
-add_action( 'wp_enqueue_scripts', 'rhd_woocommerce_scripts' );
-
-/**
  * Disable the default WooCommerce stylesheet.
  *
  * Removing the default WooCommerce stylesheet and enqueing your own will
