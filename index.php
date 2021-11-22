c42c66f22dd1119ca61b2940246b924c17fb9700
Nick Gaswirth <admin@roundhouse-designs.com>
2021-11-22 15:39:10 -0500

    launch ready

--- index.php
+++ index.php
@@ -12,22 +12,26 @@ get_header();
 
 		<?php
 		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
+			if ( is_home() ) :
+				$paged = get_query_var( 'paged' );
 				?>
 				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
+					<h1 class="page-title"><?php single_post_title(); ?> <?php echo $paged > 1 ? '&bull; ' . __( 'Page: ', 'rhd' ) . $paged : ''; ?></h1>
+				</header> <?php
 			endif;
+			?>
 
-			/* Start the Loop */
+			<div class="feed">
+			
+			<?php
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'template-parts/content', get_post_type() );
 			endwhile;
+			?>
+			</div><!-- .feed -->
 
-			the_posts_navigation();
+			<?php the_posts_navigation();
 
 		else :
 
